d4dd2754baf2a5e5eedae602a9eed2caa0e6b3f6
Allen95Wei <allen95wei@gmail.com>
2025-04-09 11:52:55 +0800

    feat: 關閉`bat`檔的指令輸出
    Signed-off-by: Allen95Wei <allen95wei@gmail.com>

--- run.bat
+++ run.bat
@@ -1,2 +1,4 @@
+@echo off
+echo 透過虛擬環境啟動伺服器...
 ".venv/Scripts/activate.bat" && python server.py
 pause
--- update.bat
+++ update.bat
@@ -1,3 +1,5 @@
+@echo off
+echo 自動更新系統中...
 git fetch --all
 git reset --hard origin/master
 git pull
