8c9965cb8b6c1f9e2e3c2baca7dbbfbee2524de3
Allen95Wei <allen95wei@gmail.com>
2025-04-11 15:46:24 +0800

    test
    Signed-off-by: Allen95Wei <allen95wei@gmail.com>

--- main.bat
+++ main.bat
@@ -3,18 +3,18 @@ chcp 65001
 goto init
 
 :init
-echo 1. 更新並執行 WS 伺服器
-echo 2. 執行 WS 伺服器
-echo 3. 更新 WS 伺服器
-echo 4. 安裝 Python 需求套件
-echo 5. 完整安裝 (虛擬環境、
+@REM echo 1. 更新並執行 WS 伺服器
+echo 1. 執行 WS 伺服器
+echo 2. 更新 WS 伺服器
+echo 3. 安裝 Python 需求套件
+echo 4. 完整安裝 (虛擬環境、
 choice /c 12345 /m "請選擇："
 
-if %ERRORLEVEL% EQU 5 goto install
-if %ERRORLEVEL% EQU 4 goto install_pkgs
-if %ERRORLEVEL% EQU 3 goto update
-if %ERRORLEVEL% EQU 2 goto run
-if %ERRORLEVEL% EQU 1 goto update_and_run
+if %ERRORLEVEL% EQU 4 goto install
+if %ERRORLEVEL% EQU 3 goto install_pkgs
+if %ERRORLEVEL% EQU 2 goto update
+if %ERRORLEVEL% EQU 1 goto run
+@REM if %ERRORLEVEL% EQU 1 goto update_and_run
 
 :update_and_run
 update.bat
