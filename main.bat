3c3eca44742ea72fd3f424fc0a9e8a1d2e06eb77
Allen95Wei <allen95wei@gmail.com>
2025-04-11 14:27:56 +0800

    feat: 更新`main.bat`
    Signed-off-by: Allen95Wei <allen95wei@gmail.com>

--- main.bat
+++ main.bat
@@ -3,36 +3,45 @@ chcp 65001
 goto init
 
 :init
-echo "1. 更新並執行 WS 伺服器"
-echo "2. 執行 WS 伺服器"
-echo "3. 更新 WS 伺服器"
-echo "4. 安裝 Python 需求套件"
-echo "5. 完整安裝 (虛擬環境、"
+echo 1. 更新並執行 WS 伺服器
+echo 2. 執行 WS 伺服器
+echo 3. 更新 WS 伺服器
+echo 4. 安裝 Python 需求套件
+echo 5. 完整安裝 (虛擬環境、
 choice /c 12345 /m "請選擇："
 
-if ERRORLEVEL 5 goto install
-if ERRORLEVEL 4 goto install_pkgs
-if ERRORLEVEL 3 goto update
-if ERRORLEVEL 2 goto run
-if ERRORLEVEL 1 goto update_and_run
-
-echo "輸入無效，請重新選擇"
-goto init
+if %ERRORLEVEL% EQU 5 goto install
+if %ERRORLEVEL% EQU 4 goto install_pkgs
+if %ERRORLEVEL% EQU 3 goto update
+if %ERRORLEVEL% EQU 2 goto run
+if %ERRORLEVEL% EQU 1 goto update_and_run
 
 :update_and_run
 update.bat
 run.bat
+goto end
 
 :run
 run.bat
+goto end
 
 :update
 update.bat
+goto end
 
 :install_pkgs
-echo "install_pkgs"
-exit
+where /q deactivate >nul 2>nul
+if %ERRORLEVEL% NEQ 0 (
+    "./.venv/Scripts/activate.bat" && pip install -U -r requirements.txt && deactivate
+) else (
+    pip install -U -r requirements.txt
+)
+goto end
 
 :install
 echo "install"
-exit
+goto end
+
+:end
+echo END
+pause
