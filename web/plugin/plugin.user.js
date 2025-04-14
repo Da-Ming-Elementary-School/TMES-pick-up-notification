afa1b219aee9fb765f171765a295566ba65a7890
Allen95Wei <allen95wei@gmail.com>
2025-04-14 17:27:06 +0800

    feat: 新增`updateURL`及`downloadURL`，可達到自動更新
    Signed-off-by: Allen95Wei <allen95wei@gmail.com>

--- web/plugin/plugin.user.js
+++ web/plugin/plugin.user.js
@@ -1,13 +1,14 @@
 // ==UserScript==
 // @name         大明學生呼叫系統 (無邊際通知)
 // @namespace    http://tampermonkey.net/
-// @version      1.0
+// @version      1.1
 // @description  即使在不同網頁，也能接收呼叫通知
-// @author       Michael Xu
-// @author       Allen Wei
+// @author       Michael Xu & Allen Wei
 // @match        https://*/*
 // @match        http://*/*
 // @grant        none
+// @updateURL    https://da-ming-elementary-school.github.io/TMES-pick-up-notification/plugin/plugin.meta.js
+// @downloadURL  https://da-ming-elementary-school.github.io/TMES-pick-up-notification/plugin/plugin.user.js
 // ==/UserScript==
 
 (function connectWS() {
