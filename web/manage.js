b9a46608b7a8ea77583af7650da8d17ed98edc20
Allen <49893966+Alllen95Wei@users.noreply.github.com>
2025-05-08 09:37:44 +0800

    feat(manage-page): 新增 `guidGenerator`
    Signed-off-by: Allen <49893966+Alllen95Wei@users.noreply.github.com>

--- web/manage.js
+++ web/manage.js
@@ -226,3 +226,10 @@ function formatStudentId(classNo, seatNo) {
         return `${classNo}${seatNo}`
     }
 }
+
+function guidGenerator() {
+    let S4 = function () {
+        return (((1 + Math.random()) * 0x10000) | 0).toString(16).substring(1);
+    };
+    return (S4() + S4() + "-" + S4() + "-" + S4() + "-" + S4() + "-" + S4() + S4() + S4());
+}
