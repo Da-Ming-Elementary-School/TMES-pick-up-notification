77f15e402c3f0a785aaf6c586ac4ca346207be80
Allen <49893966+Alllen95Wei@users.noreply.github.com>
2025-05-14 07:36:05 +0800

    feat(logger): 新增 `RemoteLogHandler`
    Signed-off-by: Allen <49893966+Alllen95Wei@users.noreply.github.com>

--- logger.py
+++ logger.py
@@ -5,6 +5,9 @@ import os
 import sys
 import datetime
 import zoneinfo
+from json import dumps
+import websockets
+from websockets.client import ClientConnection
 
 
 base_dir = os.path.dirname(sys.argv[0])
@@ -39,12 +42,34 @@ def create_logger() -> logging.Logger:
                             f"logs {datetime.datetime.now(tz=now_tz).strftime('%Y.%m.%d %H.%M.%S')}.log")
     with open(log_path, "w"):
         pass
-    f_formatter = logging.Formatter(
-        fmt="[%(asctime)s] %(levelname)-8s %(message)s",
-        datefmt="%Y-%m-%d %H:%M:%S")
     f_handler = logging.FileHandler(log_path, encoding="utf-8")
-    f_handler.setFormatter(f_formatter)
+    f_handler.setFormatter(get_file_formatter())
     logger.addHandler(f_handler)
     logger.setLevel(logging.DEBUG)
 
     return logger
+
+
+def get_file_formatter():
+    return logging.Formatter(
+        fmt="[%(asctime)s] %(levelname)-8s %(message)s",
+        datefmt="%Y-%m-%d %H:%M:%S")
+
+
+class RemoteLogHandler(logging.Handler):
+    def __init__(self, server_address: str):
+        self.ws: websockets.ClientConnection | None = None
+        try:
+            self.ws = ClientConnection(server_address)
+        except Exception as e:
+            print("WS connection error", e)
+        logging.Handler.__init__(self)
+
+    def emit(self, record: logging.LogRecord):
+        if self.ws:
+            msg = self.format(record)
+            data = dumps({
+                "type": "LOG",
+                "message": msg,
+            })
+            self.ws.send(data)
